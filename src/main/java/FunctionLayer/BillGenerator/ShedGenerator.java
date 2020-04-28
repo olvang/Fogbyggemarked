27de3983abaa937313e19985c6b56a0efbdf9d32
Oliver Vang <olv.vang@gmail.com>
2020-04-28 11:53:09 +0200

    Category 7 now calculates

--- src/main/java/FunctionLayer/BillGenerator/ShedGenerator.java
+++ src/main/java/FunctionLayer/BillGenerator/ShedGenerator.java
@@ -69,8 +69,46 @@ public class ShedGenerator {
         return null;
     }
 
-    public static ArrayList<BillLine> losholterSides(ArrayList<Category> categoriesUsedInGenerator) {
-        return null;
+    public static ArrayList<BillLine> losholterSides(ArrayList<Category> categoriesUsedInGenerator, int depthOfShed) {
+        ArrayList<BillLine> billLines = new ArrayList<>();
+        BillLine billLine;
+        Material material  = null;
+        int losholter = 0;
+
+        //We need 3 rows of losHolter for each side
+
+        //Then we need to calculate which material that best fit the depth, starting with the largest first
+        //So first we sort the materials in the category
+        ArrayList<Material> materialsSortedByLength = GeneratorUtilities.sortMaterialsByLength(categoriesUsedInGenerator.get(0).getMaterials());
+
+        //Now we can run trough each material to get what best fits
+        for (int i = materialsSortedByLength.size()-1; i > -1; i--) {
+            material =  materialsSortedByLength.get(i);
+
+            //If the the material length is bigger than the depth, we use that material losholter
+            if(material.getLength() / depthOfShed >= 1){
+                //Material found, which means we need 3 of them per side = 2*3 = 6
+                losholter = 6;
+                break;
+            }else if(i == 0){
+                //If No material found, we use the material anyway as it is the biggest we have
+                //We then calculate how many are needed to fill out the fullDepth
+                //The fullDepth is calculated by taking the depthOfShed * 6, since we need 3 for each side
+                int fullDepthCalc = depthOfShed * 6;
+
+                //We then keep on adding the material, until the fullLength has been filled
+                while (fullDepthCalc > 0){
+                    losholter++;
+                    fullDepthCalc -= material.getLength();
+                }
+            }
+        }
+
+        //Returns the billLine
+        billLine = new BillLine(material,losholter);
+        billLines.add(billLine);
+
+        return billLines;
     }
 
     public static ArrayList<BillLine> boardsForShed(ArrayList<Category> categoriesUsedInGenerator) {
