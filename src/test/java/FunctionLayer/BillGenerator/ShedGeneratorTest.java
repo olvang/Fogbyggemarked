add7ba983eb5cd1e11528f1a52e7ab9af6e586b2
Simon Jørgensen <simon.588.jorgensen@gmail.com>
2020-04-30 09:22:12 +0200

    Category 9 Shed generator

--- src/test/java/FunctionLayer/BillGenerator/ShedGeneratorTest.java
+++ src/test/java/FunctionLayer/BillGenerator/ShedGeneratorTest.java
@@ -1,8 +1,11 @@
 package FunctionLayer.BillGenerator;
 
+import Components.*;
 import FunctionLayer.BillLine;
 import FunctionLayer.Category;
+import FunctionLayer.Exceptions.ValidationFailedException;
 import FunctionLayer.LogicFacade;
+import FunctionLayer.Order;
 import org.junit.Test;
 
 import java.util.ArrayList;
@@ -22,6 +25,43 @@ public class ShedGeneratorTest {
 
         assertEquals(expectedName, billLineNoShed.get(0).getMaterial().getName());
         assertEquals(expected, billLineNoShed.get(0).getAmount());
+    }
+    @Test
+    public void testRemInSidesShedShort() throws Exception {
+
+        ArrayList<Category> categoriesUsedInGenerator = getCategoriesAvailable(new int[]{9});
+        Order order = new Order(new DepthComponent(500),
+                new HeightComponent(10),
+                new WidthComponent(550),
+                new ShedDepthComponent("200","500"),
+                new ShedWidthComponent("200","500"),
+                0 ,false);
+        ArrayList<BillLine> billLine = ShedGenerator.RemInSidesShed(categoriesUsedInGenerator, order.getShedDepth());
+
+        int exspectedAmount = 1;
+        int exspectedLength = 480;
+
+        assertEquals(exspectedAmount,billLine.get(0).getAmount());
+        assertEquals(exspectedLength,billLine.get(0).getMaterial().getLength());
+    }
+
+    @Test
+    public void testRemInSidesShedLong() throws Exception {
+
+        ArrayList<Category> categoriesUsedInGenerator = getCategoriesAvailable(new int[]{9});
+        Order order = new Order(new DepthComponent(500),
+                new HeightComponent(10),
+                new WidthComponent(550),
+                new ShedDepthComponent("500","550"),
+                new ShedWidthComponent("500","550"),
+                0 ,false);
+        ArrayList<BillLine> billLine = ShedGenerator.RemInSidesShed(categoriesUsedInGenerator, order.getShedDepth());
+
+        int exspectedAmount = 2;
+        int exspectedLength = 600;
+
+        assertEquals(exspectedAmount,billLine.get(0).getAmount());
+        assertEquals(exspectedLength,billLine.get(0).getMaterial().getLength());
     }
 
     private ArrayList<Category> getCategoriesAvailable(int[] categoryIdsUsedInGenerator) throws Exception{
