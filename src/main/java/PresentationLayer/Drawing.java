bb7745c2b94b807ad57a1ef1e064a303061570f3
Simon Jørgensen <simon.588.jorgensen@gmail.com>
2020-05-11 14:51:56 +0200

    JSP page for the drawing

--- src/main/java/PresentationLayer/Drawing.java
+++ src/main/java/PresentationLayer/Drawing.java
@@ -1,6 +1,11 @@
 package PresentationLayer;
 
 import FunctionLayer.Exceptions.CommandException;
+import FunctionLayer.Exceptions.DatabaseException;
+import FunctionLayer.Exceptions.DrawingFailedException;
+import FunctionLayer.LogicFacade;
+import FunctionLayer.Order;
+import FunctionLayer.SVGGenerator.DrawingGenerator;
 import FunctionLayer.SVGGenerator.SVG;
 
 import javax.servlet.http.HttpServletRequest;
@@ -10,10 +15,19 @@ public class Drawing extends Command{
     @Override
     String execute(HttpServletRequest request, HttpServletResponse response) throws CommandException {
 
-        SVG svg = new SVG(800, 600, "0,0,800,600",0,0);
+        SVG svg;
 
-        svg.addRect(0,0,600,780);
+        try {
+            int orderID = Integer.parseInt(request.getParameter("order_id"));
+            Order order = LogicFacade.getOrder(orderID);
+            svg = DrawingGenerator.createCarportDrawing(order);
 
+        } catch (DatabaseException e) {
+            return "request";
+
+        } catch (DrawingFailedException e) {
+            return "request";
+        }
         request.setAttribute("svgdrawing", svg.toString());
         return "Drawing";
     }
